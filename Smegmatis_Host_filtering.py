482794a2e496b7653ccc9681d6aedb8f28bf9eb4
hanana2000 <79883026+hanana2000@users.noreply.github.com>
2022-08-21 00:29:38 -0700

    Add files via upload

---
+++ Smegmatis_Host_filtering.py
@@ -0,0 +1,61 @@
+#this has to be run with python3
+
+#set this using pwd for main directory 
+WORKINGDIR = r"/nfs1/BIOMED/Danelishvili_Lab/Trimmomatic_data"
+OUTPUTDIR = r"/nfs1/BIOMED/Danelishvili_Lab/Trimmed_Deduped_Data"
+HOSTGENOME = r"smeg_sequence.fasta"
+ORIGREADSDIR = r"/nfs1/BIOMED/Danelishvili_Lab/Untrimmed_Read_Data"
+FILEPREFIX = r"Ph"
+DIVIDER = r"_"
+
+
+import os
+
+os.system(f"bwa index {HOSTGENOME}")
+
+directory = r'./'
+
+files  = sorted(os.scandir(WORKINGDIR), key=lambda e: e.name)
+
+phage_names = []
+
+for entry in files:
+  #look for any folders that start with Ph, 
+  # because my phage folders are names in the format "Ph123". 
+  # Substitute FILEPREFIX with what your phage files start with,
+  # and DIVIDER with name the name separation character
+  if entry.is_dir() and entry.name.startswith(f"{FILEPREFIX}"): 
+    phage_names.append(entry.name.split(f"{DIVIDER}")[0])
+
+print(phage_names)
+
+x = 0
+
+for phage in phage_names:
+  # the folder name variable is assigned based on the 
+  # prefix+phage number at the begining of the file name. 
+  # Replace the "DIVIDER" variable with the character 
+  # that separates the prefix from the rest of the foldername. 
+
+  # make a directory for the current phage in the output directory 
+  os.system(f"mkdir {OUTPUTDIR}/{phage}")
+  
+  #check against smegmatis genome to filter out smeg DNA, 
+  # output to a bam alignment file called {phage number}-smegmatis.bam
+  os.system(f"bwa mem -t 32 {HOSTGENOME} {WORKINGDIR}/{phage}_trimmomatic/{phage}-trimmed_1P {WORKINGDIR}/{phage}_trimmomatic/{phage}-trimmed_2P | samtools sort -@ 32 -o {WORKINGDIR}/{phage}_trimmomatic/{phage}-smegmatis.bam -")
+
+  # Use samtools to remove reads that were aligned to host genome in 
+  # the 
+  # DO NOT -f 4, use -f 12 so it will remove pairs even if only one of the pair mapped to smegmatis. 
+  os.system(f"samtools view -b -f 12 {WORKINGDIR}/{phage}_trimmomatic/{phage}-smegmatis.bam >{WORKINGDIR}/{phage}_trimmomatic/{phage}.bam")
+  os.system(f"samtools sort -@ 8 -n {WORKINGDIR}/{phage}_trimmomatic/{phage}.bam -o {WORKINGDIR}/{phage}_trimmomatic/{phage}-sorted.bam")
+
+  #convert bam file to fastq file with filtered, trimmed reads
+  os.system(f"samtools bam2fq {WORKINGDIR}/{phage}_trimmomatic/{phage}-sorted.bam >{WORKINGDIR}/{phage}_trimmomatic/{phage}-phage.fastq")
+  
+  print("\n\n\n")
+
+#run with python3 -u Smegmatis_Host_filtering.py | tee Smegmatis_Host_filtering.log
+#this will redirect all runtime output to a log file
+
+
---
+++ Trimmomatic_script.py
@@ -0,0 +1,68 @@
+#this has to be run with python3
+
+#set this using pwd for main directory 
+WORKINGDIR = r'/nfs1/BIOMED/Danelishvili_Lab/Untrimmed_Read_Data'
+OUTPUTDIR = r"/nfs1/BIOMED/Danelishvili_Lab/Trimmomatic_data"
+HOSTGENOME = r"smeg_sequence.fasta"
+FILEPREFIX = r"Ph"
+DIVIDER = r"_"
+
+FASTQCLOCATION = r"/local/cluster/fastqc/fastqc"
+TRIMMOMATICLOCATION = r"/local/cluster/Trimmomatic/trimmomatic-0.39.jar"
+
+import os
+
+# include the host genome fasta file in the same directory as this script. 
+# In this case, smeg_sequence.fasta contains the smegmatis genome (host organism)
+os.system("bwa index smeg_sequence.fasta")
+
+list_of_Ph_files = []
+
+pair_completed = False
+
+
+# This will parse through the directory where the original, 
+# untrimmed paired reads have been stored. 
+# Substitute WORKINGDIR with the directory 
+# where the original reads can be found. 
+files = sorted(os.scandir(WORKINGDIR), key=lambda e: e.name)
+
+for entry in files:
+  # add all the original read files to the list of ph files
+  if not entry.path.endswith(".fastq.gz"): continue # move on to next file if it's not .fq
+  list_of_Ph_files.append(entry.name)
+  print((entry.name.split("-")[8]).split("_")[0]) 
+#print(list_of_Ph_files)
+
+x = 0
+
+while x < len(list_of_Ph_files):
+  # assign the folder name based on the file name -- 
+  # my files specifically had the phage prefix and number at the 
+  # 8th segment of the file name, divided by "-" and then "_". 
+  # Your files may be named differently, change this line according 
+  # to your file naming format
+  folder_name = (list_of_Ph_files[x].split("-")[8]).split("_")[0] # Folder name based on the prefix of the files
+  file_1 = list_of_Ph_files[x]
+  file_2 = list_of_Ph_files[x+1]
+
+#make directory for current phage in the output directory 
+  os.system(f"mkdir {OUTPUTDIR}/{folder_name}_trimmomatic")  
+
+#run fastqc to quality check for adapters on untrimmed files 
+  os.system(f" {FASTQCLOCATION} {file_1} {file_2} --outdir=fastqc/. >fastqc/{folder_name}.log 2>&1") 
+
+# trim the files using trimmomatic. I have specified Illumina nexterea adapters,
+# but this may be different based on your sequencing method. 
+# Make sure to replace the adapter argument with the correct adapter name
+  os.system(f"java -jar {TRIMMOMATICLOCATION} PE -threads 20 -phred33 -summary {OUTPUTDIR}/{folder_name}_trimmomatic/statsSummaryFile-{folder_name}.txt -validatePairs {WORKINGDIR}/{file_1} {WORKINGDIR}/{file_2} -baseout {OUTPUTDIR}/{folder_name}_trimmomatic/{folder_name}-trimmed ILLUMINACLIP:./adapters/NexteraPE-PE.fa:2:30:10 ILLUMINACLIP:./adapters/TruSeq3-PE.fa:2:30:10 MAXINFO:30:0.8 >{OUTPUTDIR}/{folder_name}_trimmomatic/trimmomatic-{folder_name}.log 2>&1") 
+
+#fastqc quality check again to make sure nextera adapters removed
+  os.system(f" {FASTQCLOCATION} {OUTPUTDIR}/{folder_name}_trimmomatic/{folder_name}-trimmed_1P {OUTPUTDIR}/{folder_name}_trimmomatic/{folder_name}-trimmed_2P --outdir=trimmed-fastqc/. >trimmed-fastqc/{folder_name}.log") 
+
+  print("\n\n\n")
+  x+=2
+
+#run with python3 -u Trimmomatic_script.py | tee Trimmomatic_script.log
+#this will redirect all runtime output to a log file
+
